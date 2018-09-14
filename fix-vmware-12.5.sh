a49538c351f1c45848cf4c8ece42d88756e669e9
Mr.T <taal@cheetah>
2018-09-14 08:38:14 +0100

    updates

---
+++ fix-vmware-12.5.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# From https://superuser.com/questions/1328401/vmware-workstation-vmmon-broken-on-ubuntu-18-04
+VMWARE_VERSION=workstation-12.5.9
+TMP_FOLDER=/tmp/patch-vmware
+rm -fdr $TMP_FOLDER
+mkdir -p $TMP_FOLDER
+cd $TMP_FOLDER
+git clone https://github.com/mkubecek/vmware-host-modules.git
+cd $TMP_FOLDER/vmware-host-modules
+git checkout $VMWARE_VERSION
+git fetch
+make
+sudo make install
+sudo rm /usr/lib/vmware/lib/libz.so.1/libz.so.1
+sudo ln -s /lib/x86_64-linux-gnu/libz.so.1 /usr/lib/vmware/lib/libz.so.1/libz.so.1
+sudo /etc/init.d/vmware restart
