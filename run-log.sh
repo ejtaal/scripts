da6d6da720a62fe9aec241c0f1d8a1761d5073bb
Erik Taal <taal@kali-scan-01>
2020-05-15 15:06:41 +0000

    updates

---
+++ run-log.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Usage: $0 <project_name> <cmd_to_run> [<argument1>] ...
+
+WHAT="$1"
+shift
+CMD="$*"
+LOG="./${WHAT}-$(date +%Y%m%d-%H%M).scriptlog"
+
+
+echo ">>> Starting $0 ..." >> "$LOG"
+echo ">>> $WHAT" >> "$LOG"
+echo ">>> $CMD" >> "$LOG"
+echo ">>> $(date)" >> "$LOG"
+time script -f -a -c "$CMD" "$LOG"
+echo ">>> $(date)" >> "$LOG"
