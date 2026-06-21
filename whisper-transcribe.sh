0069769cf741ba9cc146dcaec2eec9122981b070
ET <t@j>
2026-06-21 10:49:51 +0100

    multi file support

--- whisper-transcribe.sh
+++ whisper-transcribe.sh
@@ -18,7 +18,7 @@ run_curl() {
 
 
 if [ -z "$1" ]; then
-	echo "Usage: $(basename $0) IP MEDIA_FILE"
+	echo "Usage: $(basename $0) IP MEDIA_FILE [ MEDIA_FILE ... ]"
 	echo
 	echo "This will transcribe MEDIA_FILE using parakeet-diarized (https://github.com/jfgonsalves/parakeet-diarized)."
 fi
@@ -26,27 +26,27 @@ fi
 HOST="$1"
 shift
 
-MEDIA_FILE="$1"
-JSON_FILE="$(echo "$1" | grep -oP '^(.*\.)')json"
-TXT_FILE="$(echo "$1" | grep -oP '^(.*\.)')txt"
+for MEDIA_FILE in "$@"; do
+	JSON_FILE="$(echo "$MEDIA_FILE" | grep -oP '^(.*\.)')json"
+	TXT_FILE="$(echo "$MEDIA_FILE" | grep -oP '^(.*\.)')txt"
+	
+	if [ -s "$JSON_FILE" ]; then
+		echo "$JSON_FILE already exists."
+	else
+		run_curl "$HOST" "$MEDIA_FILE" "$JSON_FILE"
+	fi
+	
+	cat "$JSON_FILE" \
+	| jq -r '.text' \
+	| perl -pe '
+		# Give each speaker a new line
+		s/(Speaker \d+:)/\n\1/g; 
+		s/([\.\?]) (\d+: ?)/\1\n\2/g;
+		# Change to MS-DOS file format
+		s#([^\r]*?)\n#\1\r\n#g;
+		' \
+	| cat > "$TXT_FILE"
 
-if [ -s "$JSON_FILE" ]; then
-	echo "$JSON_FILE already exists."
-else
-	run_curl "$HOST" "$MEDIA_FILE" "$JSON_FILE"
-fi
-
-cat "$JSON_FILE" \
-| jq -r '.text' \
-| perl -pe '
-	# Give each speaker a new line
-	s/(Speaker \d+:)/\n\1/g; 
-	s/([\.\?]) (\d+: ?)/\1\n\2/g;
-	# Change to MS-DOS file format
-	s#([^\r]*?)\n#\1\r\n#g;
-	' \
-| cat > "$TXT_FILE"
-
-
+done 
 
 
